aeece0cd3062b5da80cbb323f65a53a0d1292d53
Joshua Wiegand <joshwiegand55@gmail.com>
2014-07-17 23:00:30 -0400

    Slowly working through this tutorial

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.core.exceptions import ValidationError
 from lists.models import Item, List
+from django.test import TestCase
 
 
 class ListAndItemModelsTest(TestCase):
@@ -35,5 +36,6 @@ class ListAndItemModelsTest(TestCase):
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
         item = Item(list=list_, text='')
-        with self.assetRaises(ValidationError):
+        with self.assertRaises(ValidationError):
             item.save()
+            item.full_clean()
