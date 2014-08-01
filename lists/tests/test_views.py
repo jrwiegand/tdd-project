efc0d45551fa1a8c80d2201a31639d06bfdc4203
Joshua Wiegand <joshwiegand55@gmail.com>
2014-08-01 17:15:52 -0400

    Pushing through

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -131,18 +131,22 @@ class ListViewTest(TestCase):
             data={'text': ''}
         )
 
+
     def test_for_invalid_input_nothing_saved_to_db(self):
         self.post_invalid_input()
         self.assertEqual(Item.objects.count(), 0)
+
 
     def test_for_invalid_input_renders_list_template(self):
         response = self.post_invalid_input()
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.html')
 
+
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
         self.assertIsInstance(response.context['form'], ItemForm)
+
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
