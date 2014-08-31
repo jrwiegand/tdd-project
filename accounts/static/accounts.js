8e038f8b17fdfaf052f24405cc7b55af93307d02
Joshua Wiegand <joshwiegand55@gmail.com>
2014-08-31 08:55:41 -0400

    Chapter 15, on the last section now.

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -2,7 +2,9 @@ var initialize = function (navigator) {
     $('#id_login').on('click', function() {
         navigator.id.request();
     });
-    navigator.id.watch();
+    navigator.id.watch({
+        loggedInUser: user
+    });
 };
 
 window.Superlists = {
