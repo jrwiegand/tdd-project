c1511db9f69a8239f61a689862294141be6fb638
Joshua Wiegand <joshwiegand55@gmail.com>
2014-10-13 21:53:34 -0400

    quick fix

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -22,7 +22,7 @@ class MyListsTest(FunctionalTest):
         ))
 
 
-       def test_logged_in_users_lists_are_saved_as_my_lists(self):
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
         # Edith is a logged-in user
         self.create_pre_authenticated_session('edith@example.com')
 
