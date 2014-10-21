8a5808b93ef2ac9c16b75d4c7eea595f3b444971
Joshua Wiegand <joshwiegand55@gmail.com>
2014-10-20 22:08:31 -0400

    Starting chapter 19

--- lists/models.py
+++ lists/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.conf import settings
 from django.core.urlresolvers import reverse
 
 
 class List(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
+
+
+    @property
+    def name(self):
+        return self.item_set.first().text
 
 
 class Item(models.Model):
@@ -19,3 +26,4 @@ class Item(models.Model):
 
     def __str__(self):
         return self.text
+
