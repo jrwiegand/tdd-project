f9b72eeb735e7674ae465ff8c33d988232f66d60
Joshua Wiegand <joshwiegand55@gmail.com>
2014-10-27 22:59:52 -0400

    Working through the chapter

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -71,18 +71,23 @@ class NewListTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-    @patch('lists.views.List')  #1
+    @patch('lists.views.List')
     def test_list_owner_is_saved_if_user_is_authenticated(self, mockList):
-        mock_list = List.objects.create()  #2
+        mock_list = List.objects.create()
         mock_list.save = Mock()
         mockList.return_value = mock_list
         request = HttpRequest()
-        request.user = User.objects.create()  #3
+        request.user = Mock()
+        request.user.is_authenticated.return_value = True
         request.POST['text'] = 'new list item'
+
+        def check_owner_assigned():
+            self.assertEqual(mock_list.owner, request.user)
+        mock_list.save.side_effect = check_owner_assigned
 
         new_list(request)
 
-        self.assertEqual(mock_list.owner, request.user)  #4
+        mock_list.save.assert_called_once_with()
 
 
 
