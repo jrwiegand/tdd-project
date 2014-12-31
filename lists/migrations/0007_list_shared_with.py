60c387d33a5c783897212435aeab4042d10487db
Joshua Wiegand <joshwiegand55@gmail.com>
2014-12-31 16:35:18 -0500

    pep8 compliant

--- lists/migrations/0007_list_shared_with.py
+++ lists/migrations/0007_list_shared_with.py
@@ -16,7 +16,9 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='list',
             name='shared_with',
-            field=models.ManyToManyField(related_name='shared_lists', to=settings.AUTH_USER_MODEL),
+            field=models.ManyToManyField(
+                related_name='shared_lists',
+                to=settings.AUTH_USER_MODEL),
             preserve_default=True,
         ),
     ]
