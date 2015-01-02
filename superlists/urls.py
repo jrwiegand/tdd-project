7a8be57b0caa0b85a1e954db699fe226a6512b0b
Joshua Wiegand <joshwiegand55@gmail.com>
2015-01-02 10:29:57 -0500

    fix urls.py through the project

--- superlists/urls.py
+++ superlists/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-                       url(r'^$', 'lists.views.home_page', name='home'),
-                       url(r'^lists/', include('lists.urls')),
-                       url(r'^accounts/', include('accounts.urls')),
-                       )
+    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/', include('lists.urls')),
+    url(r'^accounts/', include('accounts.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
+)
