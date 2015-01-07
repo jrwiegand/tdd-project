fdc548b2e5c5c8572cac52cea859018194fdedca
Joshua Wiegand <joshwiegand55@gmail.com>
2015-01-06 22:55:35 -0500

    finally got the project working

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -4,8 +4,10 @@ from .home_and_list_pages import HomePage
 
 
 def quit_if_possible(browser):
-    try: browser.quit()
-    except: pass
+    try:
+        browser.quit()
+    except:
+        pass
 
 
 class SharingTest(FunctionalTest):
@@ -57,4 +59,3 @@ class SharingTest(FunctionalTest):
         self.browser = edith_browser
         self.browser.refresh()
         list_page.wait_for_new_item_in_list('Hi Edith!', 2)
-
