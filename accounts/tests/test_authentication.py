3ec6e4abe63dafd6b35ba28c85cbc55a391de239
Joshua Wiegand <joshwiegand55@gmail.com>
2015-02-25 21:36:26 -0500

    more flake8, busy bee

--- accounts/tests/test_authentication.py
+++ accounts/tests/test_authentication.py
@@ -3,11 +3,11 @@ from unittest.mock import patch
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-User = get_user_model()
-
 from accounts.authentication import (
     PERSONA_VERIFY_URL, PersonaAuthenticationBackend
 )
+
+User = get_user_model()
 
 
 @patch('accounts.authentication.requests.post')
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,14 +5,14 @@ from django.http import HttpRequest
 from django.test import TestCase
 from django.utils.html import escape
 from django.contrib.auth import get_user_model
-User = get_user_model()
-
+from lists.models import Item, List
+from lists.views import new_list
 from lists.forms import (
     DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
     ExistingListItemForm, ItemForm,
 )
-from lists.models import Item, List
-from lists.views import new_list
+
+User = get_user_model()
 
 
 class HomePageTest(TestCase):
