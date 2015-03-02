2d29703452bf114e1e40070358854ab43c625b27
Joshua Wiegand <joshwiegand55@gmail.com>
2015-03-01 20:41:30 -0500

    This is the final commit for a while. Moving on to bigger and better things.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 login = 'accounts.views.persona_login'
-logout = 'accounts.views.persona_login'
+logout = 'django.contrib.auth.views.logout'
 
 urlpatterns = patterns('',
                        url(r'^login$', login, name='persona_login'),
